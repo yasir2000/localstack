8f3ca0fb0f570a5f0cd00dc3ff625a1581dac153
Cesar Alvernaz <cesar.alvernaz@gmail.com>
2022-04-17 23:54:10 +0100

    wip mock api refactoring

--- localstack/services/apigateway/apigateway_listener.py
+++ localstack/services/apigateway/apigateway_listener.py
@@ -5,13 +5,11 @@ import re
 import time
 from http import HTTPStatus
 from typing import Any, Dict, Optional, Tuple, Union
-from urllib.parse import urljoin
 
 import pytz
 from jsonschema import ValidationError, validate
 from requests.models import Response
 
-from localstack import config
 from localstack.constants import (
     APPLICATION_JSON,
     HEADER_LOCALSTACK_AUTHORIZATION,
@@ -46,25 +44,25 @@ from localstack.services.apigateway.helpers import (
     make_error_response,
 )
 from localstack.services.apigateway.integration import (
+    HttpIntegration,
+    KinesisIntegration,
+    LambdaIntegration,
     MockIntegration,
     RequestTemplates,
     ResponseTemplates,
-    SnsIntegration,
-    VtlTemplate, HttpIntegration, LambdaIntegration, KinesisIntegration, SqsIntegration,
     S3Integration,
+    SnsIntegration,
+    SqsIntegration,
+    VtlTemplate,
 )
 from localstack.services.generic_proxy import ProxyListener
-from localstack.services.kinesis import kinesis_listener
 from localstack.services.stepfunctions.stepfunctions_utils import await_sfn_execution_result
-from localstack.utils import common
 from localstack.utils.analytics import event_publisher
 from localstack.utils.aws import aws_responses, aws_stack
-from localstack.utils.aws.aws_responses import (
-    request_response_stream,
-    requests_response,
-)
+from localstack.utils.aws.aws_responses import requests_response
 from localstack.utils.aws.request_context import MARKER_APIGW_REQUEST_REGION, THREAD_LOCAL
 from localstack.utils.common import camel_to_snake_case, json_safe, long_uid, to_str
+
 # set up logger
 from localstack.utils.http import add_query_params_to_url
 
